919d787ef7001b1151ab89793f0b34347b364220
OriginalKilljoy <162512500+OriginalKilljoy@users.noreply.github.com>
2024-09-06 13:53:14 +0200

    Added border

--- script.js
+++ script.js
@@ -2,20 +2,27 @@
 //text-farge, border eller noe annet. Lag en funksjon til som returnerer den endringen,
 //uten å endre det som står fra før, bare legge til.
 
-function changeLook(text, color, fontSize){
-    document.getElementById('app').innerHTML = /*HTML*/`
-    <div ${changeStyle(color, fontSize)}>${text}</div>
-    `;
+function changeLook(text, color, fontSize, borderStyle, borderSize) {
+    document.getElementById('app').innerHTML = /*HTML*/ `
+<div ${changeStyle(color, fontSize, borderStyle, borderSize)}>${text}</div>
+`;
 }
 
-function changeStyle(color, fontSize){
-    return `style="${changeBackground(color)} ${changeSize(fontSize)}"`;
+function changeStyle(color, fontSize, borderStyle, borderSize) {
+    return `style="${changeBackground(color)} ${changeSize(fontSize)} ${changeBorderStyle(borderStyle, borderSize)}";`;
 }
 
-function changeBackground(color){
-    return `background-color: ${color}`;
+function changeBackground(color) {
+    return `background-color: ${color};`;
 }
 
-function changeSize(fontSize){
-    return `font-size: ${fontSize}px`;
+function changeSize(fontSize) {
+    return `font-size: ${fontSize}px;`;
+}
+
+function changeBorderStyle(borderStyle, borderSize){
+    return `border: black ${borderStyle} ${changeBorderSize(borderSize)};`;
+}
+function changeBorderSize(borderSize){
+    return `${borderSize}px;`;
 }
